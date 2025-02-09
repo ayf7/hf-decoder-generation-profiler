e6c37a10a3a0b34251091cc28238e45bf408a929
Anthony Fei <ayf7@cornell.edu>
2025-02-09 18:52:03 -0500

    updated functions

--- functions.py
+++ functions.py
@@ -3,10 +3,24 @@ from torch import Tensor
 
 # Comparison Functions
 
-def cosine_similarity(t1: Tensor, t2: Tensor) -> float:
+def _cosine_similarity(t1: Tensor, t2: Tensor):
     t1 = torch.nn.functional.normalize(t1)
     t2 = torch.nn.functional.normalize(t2)
-    return torch.nn.functional.cosine_similarity(t1, t2).item()
+    return torch.clamp(torch.nn.functional.cosine_similarity(t1, t2), -1.0, 1.0)
+
+
+def cosine_similarity(t1: Tensor, t2: Tensor) -> float:
+    return _cosine_similarity(t1, t2).item()
+
+
+def rad_angle(t1: Tensor, t2: Tensor) -> float:
+    sim = _cosine_similarity(t1, t2)
+    return torch.acos(sim).item()
+
+
+def degree_angle(t1: Tensor, t2: Tensor) -> float:
+    sim = _cosine_similarity(t1, t2)
+    return torch.rad2deg(torch.acos(sim)).item()
 
 # Singular Functions
 
